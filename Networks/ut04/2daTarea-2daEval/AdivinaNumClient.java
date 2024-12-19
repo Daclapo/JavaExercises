500a012434e480127b00f2b761a4e45084d2f64a
David Clarkson Postigo <adaclapo8@gmail.com>
2024-12-19 17:42:56 +0100

    2nd Exercise, 2nd eval

---
+++ Networks/ut04/2daTarea-2daEval/AdivinaNumClient.java
@@ -0,0 +1,29 @@
+package es.davidclarkson.practicas.ut04.entrega1;
+
+import java.io.*;
+import java.net.*;
+
+public class AdivinaNumClient {
+	public static void main(String[] args) {
+		try (
+				// Conexión al servidor
+				Socket socket = new Socket("localhost", 12345);
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))
+		) {
+			String serverMessage;
+
+			while ((serverMessage = in.readLine()) != null) {
+				System.out.println(serverMessage);
+
+				if (serverMessage.contains("Introduce tu número:")) {
+					String userInput = reader.readLine(); // Lee la entrada del usuario
+					out.println(userInput);
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Error conectando el cliente al servidor:\n" + e.getMessage());
+		}
+	}
+}
---
+++ Networks/ut04/2daTarea-2daEval/AdivinaNumServer.java
@@ -0,0 +1,28 @@
+package es.davidclarkson.practicas.ut04.entrega1;
+
+import java.io.*;
+import java.net.*;
+
+public class AdivinaNumServer {
+	public static void main(String[] args) {
+		// Inicializa el juengo, creando el numero aleatorio que los clientes/usuarios tendran que adivinar.
+		Juego juego = new Juego();
+		System.out.println("Servidor iniciado. Número a adivinar: " + juego.getNumberToGuess());
+
+		try (ServerSocket serverSocket = new ServerSocket(12345)) {
+			int playerCount = 0;
+
+			while (!juego.isGameFinished()) {
+				Socket clientSocket = serverSocket.accept();
+				playerCount++;
+				String playerName = "Jugador " + playerCount;
+
+				System.out.println(playerName + " conectado.");
+				ClientHandler clientHandler = new ClientHandler(clientSocket, juego, playerName);
+				clientHandler.start();
+			}
+		} catch (IOException e) {
+			System.out.println("Error al iniciar el servidor: " + e.getMessage());
+		}
+	}
+}
